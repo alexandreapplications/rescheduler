3e99321dd94da4ad9677045f86492b2455b597ec
A. Torres Sampaio <a.torres.sampaio@avanade.com>
2019-04-09 17:20:02 -0300

    Deploy da nova versão dos dados

--- ngFront/src/environments/environment.prod.ts
+++ ngFront/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+    production: true,
+    webapi: {
+        resType: 'https://us-central1-sharing-schedule.cloudfunctions.net/resType',
+        resItem: 'https://us-central1-sharing-schedule.cloudfunctions.net/resItem',
+        domain: 'https://us-central1-sharing-schedule.cloudfunctions.net/domain',
+    },
 };
