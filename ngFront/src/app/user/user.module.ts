690ff6885f678e818da348a1798c6b2b0e3a3aee
Alexandre Sampaio <alexandrenomsn@hotmail.com>
2019-04-09 22:34:44 -0300

    Inclusão das novas imagens

--- ngFront/src/app/user/user.module.ts
+++ ngFront/src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserLandingComponent } from './user-landing/user-landing.component';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
-  declarations: [UserLandingComponent],
-  imports: [CommonModule, UserRoutingModule]
+    declarations: [UserLandingComponent],
+    imports: [CommonModule, UserRoutingModule, MatCardModule],
 })
 export class UserModule {}
